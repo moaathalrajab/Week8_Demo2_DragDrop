ef19d06a19443f48fddccbc8d043843485268af0
Moaath Alrajab <moaath@alrajab.net>
2018-10-25 01:12:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/moaathalrajab/week8_demo2_dragdrop/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.moaathalrajab.week8_demo2_dragdrop;
+
+import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
